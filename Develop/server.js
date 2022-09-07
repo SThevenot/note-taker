df3c2bb7d857e3d8a92fe239c2e8fd18a8a2df51
SThevenot <sktmh007@gmail.com>
2022-09-07 08:29:00 -0500

    added correct readme

--- Develop/server.js
+++ Develop/server.js
@@ -28,26 +28,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-function saveNewNote(text, noteArr) {
-  const saveNote = text;
-  if (!Array.isArray(noteArr)) noteArr = [];
-  if (noteArr.length === 0) noteArr.push(0);
 
-  text.id = noteArr[0];
-  noteArr[0]++;
-
-  noteArr.push(saveNote);
-  fs.writeFileSync(
-    path.join(__dirname, "./db/db.json"),
-    JSON.stringify(noteArr, null, 2)
-  );
-  return saveNote;
-}
-
-app.post("/api/notes", (req, res) => {
-  const saveNote = saveNewNote(req.body, notes);
-  res.json(saveNote);
-});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
