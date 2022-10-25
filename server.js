c02b19bf229980dd761bdce10e9498753c633e73
SThevenot <sktmh007@gmail.com>
2022-10-25 13:05:17 -0500

    fixed file structure so website can be properly deployed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const notesjson = require("./Develop/db/db.json");
+const notesjson = require("./db/db.json");
 
 //middleware
 app.use(express.json());
@@ -20,15 +20,15 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 function createNote(body, noteArr) {
